5aa56338872d2946cd21a68e4bcad91f8dc0e21a
Francis <francis.kim@travelflan.com>
2021-04-29 17:37:40 +0900

    Add attachments bot

--- bot.py
+++ bot.py
@@ -1,14 +1,26 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+from utils.attachments import AttachmentHandler
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
 
 
-class MyBot(ActivityHandler):
+class MyBot(AttachmentHandler, ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
+        if (
+            turn_context.activity.attachments
+            and len(turn_context.activity.attachments) > 0
+        ):
+            await self._handle_incoming_attachment(turn_context)
+        else:
+            await self._handle_outgoing_attachment(turn_context)
+
+
+        if turn_context.activity.text == "파일":
+            await self._display_options(turn_context)
         await turn_context.send_activity(f"따라하기 '{ turn_context.activity.text }'")
 
     async def on_members_added_activity(
@@ -16,6 +28,16 @@ class MyBot(ActivityHandler):
         members_added: ChannelAccount,
         turn_context: TurnContext
     ):
-        for member_added in members_added:
-            if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+        await self._send_welcome_message(turn_context)
+
+    async def _send_welcome_message(self, turn_context: TurnContext):
+        """
+        Greet the user and give them instructions on how to interact with the bot.
+        :param turn_context:
+        :return:
+        """
+        for member in turn_context.activity.members_added:
+            if member.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity(
+                    f"{member.name} 님. 저는 Francis 봇입니다."
+                )
