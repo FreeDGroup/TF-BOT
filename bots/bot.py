9286f6552c7b2bb153978d41b8cbea3f1913fa26
xncbf <xncbf12@gmail.com>
2021-05-10 12:08:27 +0900

    update helper text

--- bots/bot.py
+++ bots/bot.py
@@ -17,12 +17,12 @@ class MyBot(AttachmentsHandler, ActivityHandler):
             await self._handle_incoming_attachment(turn_context)
         else:
             if turn_context.activity.text == '/':
-                await turn_context.send_activity(
-                    "도움말:\n"
-                    + "/날씨\n"
-                    + "/미세먼지\n"
-                    + "이미지 첨부: 이미지를 전송하면 공유가능한 링크가 반환됩니다."
-                )
+                await turn_context.send_activity("""
+도움말:
+/날씨
+/미세먼지
+이미지 첨부: 이미지를 전송하면 공유가능한 링크가 반환됩니다.
+                """)
             elif turn_context.activity.text == '/날씨':
                 await turn_context.send_activity(f"TODO")
             elif turn_context.activity.text == '/미세먼지':
@@ -39,11 +39,12 @@ class MyBot(AttachmentsHandler, ActivityHandler):
 
     async def _send_welcome_message(self, turn_context: TurnContext):
         """
-        Greet the user and give them instructions on how to interact with the bot.: param turn_context:: return:
+        Greet the user and give them instructions on how to interact with the bot.
+        :param turn_context:
+        :return:
         """
         for member in turn_context.activity.members_added:
             if member.id != turn_context.activity.recipient.id:
-                print(member)
                 await turn_context.send_activity(
                     f"안녕하세요 {member.name} 님"
                 )
