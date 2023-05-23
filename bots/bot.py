ec72ed3f24786b1b53b4b97a856a9116df3909a8
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-05-23 21:01:13 +0900

    test run dialog

--- bots/bot.py
+++ bots/bot.py
@@ -9,9 +9,6 @@ from botbuilder.core.teams import TeamsActivityHandler
 from botbuilder.dialogs import Dialog
 from botbuilder.schema import ChannelAccount
 
-from bots.handlers.attachments import AttachmentsHandler
-from bots.handlers.helpers import HelpersHandler
-from bots.handlers.meetings import MeetingsHandler
 from bots.handlers.welcomes import WelcomesHandler
 from utils.dialog_helper import DialogHelper
 
@@ -35,13 +32,6 @@ class MyBot(WelcomesHandler, TeamsActivityHandler):
         # Save any state changes that might have occurred during the turn.
         await self.conversation_state.save_changes(turn_context, False)
         await self.user_state.save_changes(turn_context, False)
-
-    # async def on_message_activity(self, turn_context: TurnContext):
-    #     await DialogHelper.run_dialog(
-    #         self.dialog,
-    #         turn_context,
-    #         self.conversation_state.create_property("DialogState"),
-    #     )
 
     async def on_members_added_activity(self, members_added: List[ChannelAccount], turn_context: TurnContext):
         for member in members_added:
@@ -69,25 +59,32 @@ class MyBot(WelcomesHandler, TeamsActivityHandler):
             self.conversation_state.create_property("DialogState"),
         )
 
+    # async def on_message_activity(self, turn_context: TurnContext):
+    #     if (
+    #         turn_context.activity.attachments
+    #         and len(turn_context.activity.attachments) > 0
+    #         and turn_context.activity.attachments[0].content_type != "text/html"
+    #     ):
+    #         await AttachmentsHandler().handle_incoming_attachment(turn_context)
+    #     else:
+    #         if "<at>Francis 봇</at>" in turn_context.activity.text:
+    #             text = turn_context.activity.text.split("<at>Francis 봇</at>")[1].strip()
+    #         else:
+    #             text = turn_context.activity.text
+    #         if text == "/":
+    #             await HelpersHandler().send_help_message(turn_context)
+    #         elif text.startswith("/회의실"):
+    #             await MeetingsHandler().handle(turn_context)
+    #         elif text == "/날씨":
+    #             await turn_context.send_activity("TODO")
+    #         elif text == "/미세먼지":
+    #             await turn_context.send_activity("TODO")
+    #         else:
+    #             await turn_context.send_activity("아직 등록되지 않은 명령어입니다. 도움이 필요하시면 `/` 를 입력해주세요")
+
     async def on_message_activity(self, turn_context: TurnContext):
-        if (
-            turn_context.activity.attachments
-            and len(turn_context.activity.attachments) > 0
-            and turn_context.activity.attachments[0].content_type != "text/html"
-        ):
-            await AttachmentsHandler().handle_incoming_attachment(turn_context)
-        else:
-            if "<at>Francis 봇</at>" in turn_context.activity.text:
-                text = turn_context.activity.text.split("<at>Francis 봇</at>")[1].strip()
-            else:
-                text = turn_context.activity.text
-            if text == "/":
-                await HelpersHandler().send_help_message(turn_context)
-            elif text.startswith("/회의실"):
-                await MeetingsHandler().handle(turn_context)
-            elif text == "/날씨":
-                await turn_context.send_activity("TODO")
-            elif text == "/미세먼지":
-                await turn_context.send_activity("TODO")
-            else:
-                await turn_context.send_activity("아직 등록되지 않은 명령어입니다. 도움이 필요하시면 `/` 를 입력해주세요")
+        await DialogHelper.run_dialog(
+            self.dialog,
+            turn_context,
+            self.conversation_state.create_property("DialogState"),
+        )
