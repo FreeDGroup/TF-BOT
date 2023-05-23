fc08617dc14fb89b8458a3cb52c0ff4bfac2e36d
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-05-23 14:32:02 +0900

    remove url

--- bots/handlers/oauth.py
+++ bots/handlers/oauth.py
@@ -14,14 +14,7 @@ async def send_oauth_card(turn_context):
     card = OAuthCard(
         text="로그인이 필요합니다.",
         connection_name=connection_name,
-        buttons=[
-            CardAction(
-                title="로그인",
-                type=ActionTypes.signin,
-                value=f"https://login.microsoftonline.com/{CONFIG.TENANT_ID}/oauth2/v2.0/authorize?response_type=code&client_id=a795c2b9-3815-4cda-b80a-0d0cb8ad6109&redirect_uri=https://token.botframework.com/.auth/web/redirect&state=12345&prompt=consent&scope=Calendars.Read"
-                # 이 value 값은 실제 OAuth 설정에 맞게 변경해야 합니다.
-            )
-        ],
+        buttons=[CardAction(type=ActionTypes.signin, title="로그인", value="signin")],
     )
 
     # 메시지에 OAuthCard를 추가하고 사용자에게 보냅니다.
