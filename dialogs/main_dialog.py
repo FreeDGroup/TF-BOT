8a248a32aabc6f8f8b35d75f6e78bd731d98ef8f
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-05-28 13:58:23 +0900

    remove: dialog if success login

--- dialogs/main_dialog.py
+++ dialogs/main_dialog.py
@@ -35,8 +35,6 @@ class MainDialog(LogoutDialog):
                 [
                     self.prompt_step,
                     self.login_step,
-                    # self.display_token_phase1,
-                    # self.display_token_phase2,
                 ],
             )
         )
@@ -47,36 +45,6 @@ class MainDialog(LogoutDialog):
         return await step_context.begin_dialog(OAuthPrompt.__name__)
 
     async def login_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
-        # Get the token from the previous step. Note that we could also have gotten the
-        # token directly from the prompt itself. There is an example of this in the next method.
-        if step_context.result:
-            await step_context.context.send_activity("로그인 성공.")
-            # return await step_context.prompt(
-            #     ConfirmPrompt.__name__,
-            #     PromptOptions(prompt=MessageFactory.text("Would you like to view your token?")),
-            # )
-        else:
+        if not step_context.result:
             await step_context.context.send_activity("로그인에 실패했습니다 다시 시도해주세요.")
-        return await step_context.end_dialog()
-
-    async def display_token_phase1(self, step_context: WaterfallStepContext) -> DialogTurnResult:
-        await step_context.context.send_activity("Thank you.")
-
-        if step_context.result:
-            # Call the prompt again because we need the token. The reasons for this are:
-            # 1. If the user is already logged in we do not need to store the token locally in the bot and worry
-            #    about refreshing it. We can always just call the prompt again to get the token.
-            # 2. We never know how long it will take a user to respond. By the time the
-            #    user responds the token may have expired. The user would then be prompted to login again.
-            #
-            # There is no reason to store the token locally in the bot because we can always just call
-            # the OAuth prompt to get the token or get a new token if needed.
-            return await step_context.begin_dialog(OAuthPrompt.__name__)
-
-        return await step_context.end_dialog()
-
-    async def display_token_phase2(self, step_context: WaterfallStepContext) -> DialogTurnResult:
-        if step_context.result:
-            await step_context.context.send_activity(f"Here is your token {step_context.result.token}")
-
         return await step_context.end_dialog()
