c5b9aebd3a65eb8754b9ffbe11fdf92af227e6b2
Joon Hwan 김준환 <xncbf12@gmail.com>
2023-05-29 18:03:38 +0900

    fix: time

--- utils/graph.py
+++ utils/graph.py
@@ -10,11 +10,11 @@ def get_meetings(token, user_id, start_time=None, end_time=None):
     if end_time is None:
         end_time = start_time + timedelta(days=7)  # default to one week of meetings
 
-    start_time = start_time.isoformat() + "Z"
-    end_time = end_time.isoformat() + "Z"
+    start_time = start_time.isoformat()
+    end_time = end_time.isoformat()
 
     url = f"https://graph.microsoft.com/v1.0/users/{user_id}/calendarview"
-    query_params = {"startdatetime": start_time, "enddatetime": end_time}
+    query_params = {"startDateTime": start_time, "endDateTime": end_time}
     headers = {"Authorization": f"Bearer {token}", "Prefer": 'outlook.timezone="Pacific Standard Time"'}
     response = requests.get(url, params=query_params, headers=headers)
     try:
